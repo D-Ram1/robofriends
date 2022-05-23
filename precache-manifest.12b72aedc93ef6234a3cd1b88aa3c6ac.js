38f93a684a50cb893d07fc4a93d7e76014902492
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-05-23 15:17:45 -0700

    Updates

---
+++ precache-manifest.12b72aedc93ef6234a3cd1b88aa3c6ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f4043dd298e7e2e044",
+    "url": "/robofriends/static/css/main.a2dd6f45.chunk.css"
+  },
+  {
+    "revision": "76f4043dd298e7e2e044",
+    "url": "/robofriends/static/js/main.76f4043d.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "3fece978a9cc9197c0f5",
+    "url": "/robofriends/static/css/2.82634181.chunk.css"
+  },
+  {
+    "revision": "3fece978a9cc9197c0f5",
+    "url": "/robofriends/static/js/2.3fece978.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "f5bef3b68ff1636e5afb89208c04e9da",
+    "url": "/robofriends/index.html"
+  }
+];
